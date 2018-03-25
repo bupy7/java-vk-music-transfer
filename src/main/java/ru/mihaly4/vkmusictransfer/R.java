332d770069ec4489b2b224a640ba83fe3b49242d
Vasily Belosludcev <bupy765@gmail.com>
2018-03-25 13:06:35 +0500

    more code

--- src/main/java/ru/mihaly4/vkmusictransfer/R.java
+++
@@ -1,9 +0,0 @@
-package ru.mihaly4.vkmusictransfer;
-
-public final class R {
-    // public static final String ICON_EXAMPLE = "/icons/example-32px.png";
-
-    private R() {
-        throw new AssertionError("Creating new instance is disabled.");
-    }
-}
